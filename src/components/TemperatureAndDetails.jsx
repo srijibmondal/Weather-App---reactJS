d8b879d15fd7a8728af7918fb52ecb81fbf78c31
dev_srijib <104887638+srijibmondal@users.noreply.github.com>
2024-05-05 15:48:01 +0530

    Repository Created

---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  UilArrowUp,
+  UilArrowDown,
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from "@iconscout/react-unicons";
+import { formatToLocalTime, iconUrlFormCode } from "../serivces/weatherService";
+
+function TemperatureAndDetails({weather : {details, icon, temp, temp_min, temp_max, sunrise, sunset, speed , humidity, feels_like, tiemzone}}) {
+  return (
+    <>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>{details}</p>
+      </div>
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img
+          src={iconUrlFormCode(icon)}
+          alt=""
+          className="w-20"
+        />
+        <p className="text-5xl">{`${temp.toFixed()}º`}</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTemperature size={18} className="mr-1" />
+            Real Feel:
+            <span className="font-medium ml-1">{`${feels_like.toFixed()}º`}</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">{`${humidity.toFixed()}%`}</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1" />
+            Wind:
+            <span className="font-medium ml-1">{`${speed.toFixed()} km/h`}</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise:<span className="font-medium ml-1">{formatToLocalTime(sunrise, tiemzone, "hh:mm a")}</span>
+        </p>
+        <p className="font-light">|</p>
+        <UilSunset />
+        <p className="font-light">
+          Set:<span className="font-medium ml-1">{formatToLocalTime(sunset, tiemzone, "hh:mm a")}</span>
+        </p>
+        <p className="font-light">|</p>
+        <UilSun />
+        <p className="font-light">
+          High:<span className="font-medium ml-1">{`${temp_max.toFixed()}º`}</span>
+        </p>
+        <p className="font-light">|</p>
+        <UilSun />
+        <p className="font-light">
+          Low:<span className="font-medium ml-1">{`${temp_min.toFixed()}º`}</span>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TemperatureAndDetails;
